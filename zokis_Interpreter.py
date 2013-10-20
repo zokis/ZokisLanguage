aabc0d6b7de80d4f0f1b76ae5ead6f931faf4e3c
Marcelo Tambalo <marcelo.zokis@gmail.com>
2013-10-20 17:15:19 -0200

    add ZokisCode. creates an object 'code' that can be iterated by ZokisIterable

--- zokis_Interpreter.py
+++ zokis_Interpreter.py
@@ -14,3 +14,22 @@ class ZokisIterable(object):
 
     def send(self, linha):
         self.linha_atual = linha
+
+
+class ZokisCode(object):
+
+    def __init__(self, code):
+        self.__code = code
+
+    def __len__(self):
+        return len(self.__code)
+
+    def __iter__(self):
+        return ZokisIterable(self)
+
+    def __getitem__(self, linha):
+        if linha <= len(self) - 1:
+            self.linha_atual = int(linha + 1)
+            return self.__code[self.linha_atual - 1]
+        else:
+            raise IndexError()
