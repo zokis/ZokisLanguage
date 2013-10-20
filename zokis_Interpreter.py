52d2ec15503f9bf06c29dd7d7d85a623e7f8faf2
Marcelo Tambalo <marcelo.zokis@gmail.com>
2013-10-20 17:21:54 -0200

    creates the interpreter

--- zokis_Interpreter.py
+++ zokis_Interpreter.py
@@ -1,3 +1,7 @@
+# coding: utf-8
+from zokis_VM import ZokisVM
+
+
 class ZokisIterable(object):
 
     def __init__(self, code):
@@ -42,3 +46,27 @@ class ZokisCode(object):
             return self.__code[self.linha_atual - 1]
         else:
             raise IndexError()
+
+
+class ZokisInterpreter(object):
+
+    def __init__(self, code):
+        self.__code = code
+
+    def run(self):
+        z_iter = iter(ZokisCode(self.__code))
+        vm = ZokisVM(z_iter)
+        while True:
+            try:
+                linha = z_iter.next()
+                if '#' in linha:
+                    code, comment = linha.split('#', 1)
+                    code = code.strip()
+                else:
+                    code = linha.strip()
+            except StopIteration:
+                break
+            if code.isdigit():
+                vm.PUSH(float(code))
+            elif code:
+                getattr(vm, code, vm.NOOP)()
